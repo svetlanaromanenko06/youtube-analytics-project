40f46e48e8efb01b7baac2bcbe5e5ba24348e7a8
svetlana romanenko <svroman07@gmail.com>
2023-05-31 22:52:53 +0300

    Исправлена ошибка в self.url

--- src/channel.py
+++ src/channel.py
@@ -4,18 +4,48 @@ from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
-
 class Channel:
-    """Класс для ютуб-канала"""
+
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+
+        self.__channel_id = channel_id
+        youtube_channels = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics')
+        self.__info_to_print = youtube_channels.execute()
+        self.title = self.__info_to_print["items"][0]["snippet"]["title"]
+        self.description = self.__info_to_print["items"][0]["snippet"]["description"]
+        self.subscriber_count: int = int(self.__info_to_print["items"][0]["statistics"]["subscriberCount"])
+        self.video_count: int = self.__info_to_print["items"][0]["statistics"]["videoCount"]
+        self.view_count: int = self.__info_to_print["items"][0]["statistics"]["viewCount"]
+        self.url = f"https://www.youtube.com/channel/{self.__info_to_print['items'][0]['id']}"
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        info = json.dumps(channel, indent=2, ensure_ascii=False)
-        print(info)
+        print(json.dumps(self.__info_to_print, indent=2, ensure_ascii=False))
+
+
+    def to_json(self, filename):
+        """Сохраняет значения атрибутов экземпляра Channel в файл в формате JSON."""
+        channel = {
+                'channel_id': self.__channel_id,
+                'title': self.title,
+                'description': self.description,
+                'customUrl': self.url,
+                'subscriberCount': self.subscriber_count,
+                'videoCount': self.video_count,
+                'viewCount': self.view_count
+                }
+        with open(filename, "w", encoding="UTF-8") as file:
+            json.dump(channel, file, ensure_ascii=False)
 
